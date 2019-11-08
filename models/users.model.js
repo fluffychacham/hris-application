d115945a8ba2a302923d9c2baf227e34c13c1f94
Alan Perez <alanp9six@gmail.com>
2019-11-07 19:18:35 -0600

    schema for model.. wip

---
+++ models/users.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  role: {
+    type: String
+  },
+})
+
+
+mongoose.model('Users', userSchema)
