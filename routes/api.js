35e5e748c6fa4972315549dd80e515215a1c3159
fluffychacham <chamtejido@gmail.com>
2019-11-17 22:35:35 -0700

    removed graphql from api.js

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,3 @@
 const schema = require("../middlewares/gql");
-const graphqlHttp = require("express-graphql");
 
-module.exports = function(server) {
-  server.use(
-    "/api",
-    graphqlHttp({
-      schema: schema,
-      graphiql: true
-    })
-  );
-};
+module.exports = function(server) {};
