61d5a9609ad0bfa0a69a901a6d33e77787227faf
fluffychacham <chamtejido@gmail.com>
2019-11-17 22:50:50 -0700

    removed MongoDB support

--- models/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongo = require("mongoose");
-
-// Get MongoURI
-const db = require("../config/keys").MongoURI;
-
-mongo
-  .connect(db, { useNewUrlParser: true })
-  .then(console.log(`MongoDB Connected!`))
-  .catch(err => console.log(`MongoDB Error: ${err}`));
