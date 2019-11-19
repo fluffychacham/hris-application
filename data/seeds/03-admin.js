7d6b525644a95ff7da1a143218d2fa75fa19d5e7
Alan Perez <alanp9six@gmail.com>
2019-11-19 12:04:09 -0600

    dummy data for admin set

--- data/seeds/03-admin.js
+++ data/seeds/03-admin.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('admin').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('admin').insert([
+        {id: 1, company_id: 1, role_id: 1,  username: 'chester123', first_name: 'Chester', last_name: 'T..', email: 'chester@mail.com'},
+        {id: 2, company_id: 2, role_id: 2,  username: 'patrick123', first_name: 'Patrick', last_name: 'D..', email: 'patrick@mail.com'},
+        {id: 3, company_id: 3, role_id: 3,  username: 'roman123', first_name: 'Roman', last_name: 'B..', email: 'roman@mail.com'}
       ]);
     });
 };
