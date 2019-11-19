1db8dfdc34dcb9203166d2323876805e6333a144
fluffychacham <chamtejido@gmail.com>
2019-11-18 18:11:46 -0700

    merged from postgres-setup branch to postgre-setup

---
+++ knexfile.js
@@ -0,0 +1,76 @@
+// Update with your config settings.
+require('dotenv').config({ path: '/.env'})
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'postgres',
+      user: 'postgres',
+      password: 'postgres',
+      host: 'localhost',
+      port: 5432
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+  test: {
+    client: 'pg',
+    connection: {
+      database: 'postgres',
+      user: 'postgres',
+      password: 'postgres',
+      host: 'localhost',
+      port: 5432
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+  staging: {
+    client: 'pg',
+    // eslint-disable-next-line linebreak-style
+    // deploy stage
+    connection: {
+      host: process.env.S_HOST,
+      user: process.env.S_USER,
+      password: process.env.S_PASS,
+      database: process.env.S_DB
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.PG_HOST,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      database: process.env.PG_DB
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+};
