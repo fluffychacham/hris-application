191ee96ede4962fdd18ef5664580217bbe43de40
fluffychacham <chamtejido@gmail.com>
2019-11-19 11:23:42 -0700

    fixed seed errors

--- data/seeds/05-employee.js
+++ data/seeds/05-employee.js
@@ -1,13 +1,37 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("employee")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, company_id: 1 , role_id: 1, first_name: "Joom", last_name: "Boom", email: "joom@mail.com", password: "joom123"},
-        {id: 2, company_id: 2 , role_id: 2, first_name: "Nick", last_name: "Doom", email: "nick@mail.com", password: "nick123"},
-        {id: 3, company_id: 3 , role_id: 3, first_name: "Billy", last_name: "Block", email: "billy@mail.com", password: "billy123"}
+      return knex("employee").insert([
+        {
+          id: 1,
+          company_id: 1,
+          role_id: 1,
+          first_name: "Joom",
+          last_name: "Boom",
+          email: "joom@mail.com",
+          password: "joom123"
+        },
+        {
+          id: 2,
+          company_id: 2,
+          role_id: 2,
+          first_name: "Nick",
+          last_name: "Doom",
+          email: "nick@mail.com",
+          password: "nick123"
+        },
+        {
+          id: 3,
+          company_id: 3,
+          role_id: 3,
+          first_name: "Billy",
+          last_name: "Block",
+          email: "billy@mail.com",
+          password: "billy123"
+        }
       ]);
     });
 };
