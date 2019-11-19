2c300351bdf948d967f4a989a5cea00e8bd2783f
Alan Perez <alanp9six@gmail.com>
2019-11-19 12:11:14 -0600

    dummy data set

--- data/seeds/04-hr.js
+++ data/seeds/04-hr.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('hr').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, company_id: 1 , role_id: 1, first_name: "Tony", last_name: "Montana", email: "tony@mail.com", password: "tony123"},
+        {id: 2, company_id: 2 , role_id: 2, first_name: "Jake", last_name: "Rat", email: "rat@mail.com", password: "rat123"},
+        {id: 3, company_id: 3 , role_id: 3, first_name: "Holly", last_name: "Smith", email: "holly@mail.com", password: "holly123"}
       ]);
     });
 };
