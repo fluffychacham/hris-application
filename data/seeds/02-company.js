9de466b0ed7747f73b24a6f8c59211d6441038b5
Alan Perez <alanp9six@gmail.com>
2019-11-19 12:15:59 -0600

    removed data from seed

--- data/seeds/02-company.js
+++ data/seeds/02-company.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('company').insert([
-        {id: 1, name: 'Fedex Ground', domain: 'www.fedex.com', date_created:''},
-        {id: 2, name: 'UPS', domain: 'www.ups.com', date_created:''},
-        {id: 3, name: 'Coders Inc', domain: 'www.google.com', date_created:''}
+        {id: 1, name: 'Fedex Ground', domain: 'www.fedex.com', },
+        {id: 2, name: 'UPS', domain: 'www.ups.com', },
+        {id: 3, name: 'Coders Inc', domain: 'www.google.com', }
       ]);
     });
 };
--- data/seeds/05-employee.js
+++ data/seeds/05-employee.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('employee').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('employee').insert([
         {id: 1, company_id: 1 , role_id: 1, first_name: "Joom", last_name: "Boom", email: "joom@mail.com", password: "joom123"},
         {id: 2, company_id: 2 , role_id: 2, first_name: "Nick", last_name: "Doom", email: "nick@mail.com", password: "nick123"},
         {id: 3, company_id: 3 , role_id: 3, first_name: "Billy", last_name: "Block", email: "billy@mail.com", password: "billy123"}
