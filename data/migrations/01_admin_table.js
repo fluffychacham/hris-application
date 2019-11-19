937d0cef52f584f8296751a0061edf3fb19cccb1
fluffychacham <chamtejido@gmail.com>
2019-11-19 10:53:28 -0700

    removed mongoose db models

--- data/migrations/01_admin_table.js
+++ data/migrations/01_admin_table.js
@@ -1,38 +1,37 @@
-exports.up = function (knex) {
-  return knex.schema.createTable('admin', (tbl) => {
+exports.up = function(knex) {
+  return knex.schema.createTable("admin", tbl => {
     tbl.increments();
 
-    tbl.integer('company_id')
+    tbl
+      .integer("company_id")
       .unsigned()
       .notNullable()
-      .references('id')
-      .inTable('company')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE');
-    
-    tbl.integer('role_id')
+      .references("id")
+      .inTable("company")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+
+    tbl
+      .integer("role_id")
       .unsigned()
       .notNullable()
-      .references('id')
-      .inTable('role')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE');
-    tbl.string('username', 128)
+      .references("id")
+      .inTable("role")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    tbl
+      .string("username", 128)
       .unique()
       .notNullable();
+    tbl.string("first_name", 32).notNullable();
+    tbl.string("last_name", 32).notNullable();
     tbl
-      .string('first_name', 32)
-      .notNullable();
-    tbl
-      .string('last_name' 32)
-      .notNullable();
-    tbl
-      .string('email', 256)
+      .string("email", 256)
       .unique()
       .notNullable();
   });
 };
 
-exports.down = function (knex) {
-  return knex.schema.dropTableIfExists('admin');
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("admin");
 };
